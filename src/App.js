fed62a3520cf6e1ecc2e9933a64786b8a54967a0
Mihai-Daniel Radu <mihaidaniel.radu00@gmail.com>
2021-01-08 16:08:59 +0200

    Added local storage functionality

--- src/App.js
+++ src/App.js
@@ -5,55 +5,72 @@ import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
-    //States
-    const [inputText, setInputText] = useState("");
-    const [todos, setTodos] = useState([]);
-    const [status, setStatus] = useState("all");
-    const [filteredTodos, setFilteredTodos] = useState([]);
+	//States
+	const [inputText, setInputText] = useState("");
+	const [todos, setTodos] = useState([]);
+	const [status, setStatus] = useState("all");
+	const [filteredTodos, setFilteredTodos] = useState([]);
+	//Run Once when app starts
+	useEffect(() => {
+		getLocalTodos();
+	}, []);
+	//Use Effect
+	useEffect(() => {
+		filterHandler();
+		saveLocalTodos();
+	}, [todos, status]);
 
-    //Use Effect
-    useEffect(() => {
-        filterHandler();
-    }, [todos, status]);
+	//Functions
+	const filterHandler = () => {
+		switch (status) {
+			case "completed":
+				setFilteredTodos(
+					todos.filter((todo) => todo.completed === true)
+				);
+				break;
+			case "uncompleted":
+				setFilteredTodos(
+					todos.filter((todo) => todo.completed === false)
+				);
+				break;
+			default:
+				setFilteredTodos(todos);
+				break;
+		}
+	};
 
-    //Functions
-    const filterHandler = () => {
-        switch (status) {
-            case "completed":
-                setFilteredTodos(
-                    todos.filter((todo) => todo.completed === true)
-                );
-                break;
-            case "uncompleted":
-                setFilteredTodos(
-                    todos.filter((todo) => todo.completed === false)
-                );
-                break;
-            default:
-                setFilteredTodos(todos);
-                break;
-        }
-    };
+	//Save to Local Storage
+	const saveLocalTodos = () => {
+		localStorage.setItem("todos", JSON.stringify(todos));
+	};
+	const getLocalTodos = () => {
+		if (localStorage.getItem("todos") === null) {
+			localStorage.setItem("todos", JSON.stringify([]));
+		} else {
+			let todoLocal = JSON.parse(localStorage.getItem("todos"));
+			setTodos(todoLocal);
+		}
+	};
 
-    return (
-        <div className="App">
-            <header>
-                <h1>Your Todo List</h1>
-            </header>
-            <Form
-                inputText={inputText}
-                setInputText={setInputText}
-                todos={todos}
-                setTodos={setTodos}
-                setStatus={setStatus}
-            />
-            <TodoList
-                todos={todos}
-                setTodos={setTodos}
-                filteredTodos={filteredTodos}
-            />
-        </div>
-    );
+	return (
+		<div className="App">
+			<header>
+				<h1>Your Todo List</h1>
+			</header>
+			<Form
+				inputText={inputText}
+				setInputText={setInputText}
+				todos={todos}
+				setTodos={setTodos}
+				setStatus={setStatus}
+			/>
+			<TodoList
+				todos={todos}
+				setTodos={setTodos}
+				filteredTodos={filteredTodos}
+			/>
+		</div>
+	);
 }
 
 export default App;
