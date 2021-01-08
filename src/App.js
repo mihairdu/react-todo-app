6cdf5dccedc8564c8d9bb9598676bd2fca594f81
Mihai-Daniel Radu <mihaidaniel.radu00@gmail.com>
2021-01-08 14:47:10 +0200

    Added input text

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 //importing components
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>Your Todo List</h1>
-      </header>
-      <Form />
-      <TodoList />
-    </div>
-  );
+    const [inputText, setInputText] = useState("");
+    const [todos, setTodos] = useState([]);
+    return (
+        <div className="App">
+            <header>
+                <h1>Your Todo List</h1>
+            </header>
+            <Form
+                inputText={inputText}
+                setInputText={setInputText}
+                todos={todos}
+                setTodos={setTodos}
+            />
+            <TodoList />
+        </div>
+    );
 }
 
 export default App;
