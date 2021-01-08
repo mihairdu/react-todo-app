78192ed83a39eede587ec7f654570eff524b0bd6
Mihai-Daniel Radu <mihaidaniel.radu00@gmail.com>
2021-01-08 13:42:45 +0200

    Initial project commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Your Todo List</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
