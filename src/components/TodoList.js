1600777170e910a0ffd18a7efff009bab2974903
Mihai-Daniel Radu <mihaidaniel.radu00@gmail.com>
2021-01-08 16:11:06 +0200

    Finished todos functionalities

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,21 +3,21 @@ import React from "react";
 import Todo from "./Todo";
 
 const TodoList = ({ todos, setTodos, filteredTodos }) => {
-    return (
-        <div className="todo-container">
-            <ul className="todo-list">
-                {filteredTodos.map((todo) => (
-                    <Todo
-                        key={todo.id}
-                        text={todo.text}
-                        todo={todo}
-                        todos={todos}
-                        setTodos={setTodos}
-                    />
-                ))}
-            </ul>
-        </div>
-    );
+	return (
+		<div className="todo-container">
+			<ul className="todo-list">
+				{filteredTodos.map((todo) => (
+					<Todo
+						key={todo.id}
+						text={todo.text}
+						todo={todo}
+						todos={todos}
+						setTodos={setTodos}
+					/>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default TodoList;
