f01ecb17868dd1abcd89fdfa5bde88973822b2e5
Mihai-Daniel Radu <mihaidaniel.radu00@gmail.com>
2021-01-08 14:46:36 +0200

    Added input text

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,44 @@
 import React from "react";
 
-const Form = () => {
-  return (
-    <form>
-      <input type="text" className="todo-input" />
-      <button type="submit" className="todo-button">
-        <i className="fas fa-plus-square"></i>
-      </button>
-      <div className="select">
-        <select name="todos" className="filter-todos">
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-        </select>
-      </div>
-    </form>
-  );
+const Form = ({ inputText, setInputText, todos, setTodos }) => {
+    //js code and functions
+    const inputTextHandler = (e) => {
+        console.log(e.target.value);
+        setInputText(e.target.value);
+    };
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        setTodos([
+            ...todos,
+            { text: inputText, completed: false, id: Math.random() * 1000 },
+        ]);
+        setInputText("");
+    };
+
+    return (
+        <form>
+            <input
+                value={inputText}
+                onChange={inputTextHandler}
+                type="text"
+                className="todo-input"
+            />
+            <button
+                onClick={submitTodoHandler}
+                type="submit"
+                className="todo-button"
+            >
+                <i className="fas fa-plus-square"></i>
+            </button>
+            <div className="select">
+                <select name="todos" className="filter-todos">
+                    <option value="all">All</option>
+                    <option value="completed">Completed</option>
+                    <option value="uncompleted">Uncompleted</option>
+                </select>
+            </div>
+        </form>
+    );
 };
 
 export default Form;
